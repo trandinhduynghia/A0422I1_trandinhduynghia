3d76eb147d2591ce7c3a827b84b5211e8e44dcb4
trandinhduynghia <trandinhduynghia@gmail.com>
2022-07-08 19:14:44 +0700

    capnhat

---
+++ module2/1_gioi_thieu_ve_java/thuc_hanh/src/hien_thi_thoi_gian_he_thong.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class hien_thi_thoi_gian_he_thong {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Now is: "+ now);
+    }
+}
